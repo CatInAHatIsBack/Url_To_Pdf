ae7ffb2d9149e93c1eb37e6401b21612447bec3e
Cat Hat <cat@Cats-Laptop.local>
2023-04-04 11:40:25 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::error::Error;
+use std::{env, fs};
+use headless_chrome::Browser;
+use std::io;
+
+fn browse_wikipedia(url: &str,name: &str) -> Result<(), Box<dyn Error>> {
+    let browser = Browser::default()?;
+
+    let tab = browser.new_tab()?;
+
+    let pdf = tab
+        .navigate_to(url)?
+        .wait_until_navigated()?
+        .print_to_pdf(None)?;
+    let newpath = "./pdf_folder/".to_string() + &name + ".pdf";
+    fs::write(newpath, pdf)?;
+    println!("PDF successfully created from internet web page.");
+
+    
+    Ok(())
+}
+fn main() {
+    let mut url = String::new();
+    println!("enter url");
+    io::stdin().read_line(&mut url).expect("Failed to read line");
+    url = url.trim().to_string();
+
+    let mut name = String::new();
+    println!("enter name");
+    io::stdin().read_line(&mut name).expect("Failed to read line");
+    name = name.trim().to_string();
+
+    // let mut name= "https://en.wikipedia.org/wiki/WebKit";
+    
+    browse_wikipedia(&url,&name);
+}
